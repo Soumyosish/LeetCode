f144e1b6ce7d05e18c0f2c58c1e7d4bd38fa1484
SoumyosishPal <soumyosishpal.108@gmail.com>
2025-12-01 18:50:25 +0530

    modified the comments

--- Day 1/sortcolors.java
+++ Day 1/sortcolors.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class sortcolors {
     public static void sort(int arr[]){
-        int x=0,y=0,z=0;
+        int x=0,y=0,z=0;  //initializing the colors to 0 for matching the constraints and without using Array's inbuilt sort function to sort them
         int start=0;
         int end=arr.length-1;
         while(start<=end){
-            if(arr[start]==0){
+            if(arr[start]==0){  // searching the elements whether they are present or not and if present increase the counter
                 x++;
             }
             else if(arr[start]==1){
@@ -16,8 +16,8 @@ public class sortcolors {
             }
             start++;
         }
-        for(int i=0;i<arr.length;i++){
-            if(x!=0){
+        for(int i=0;i<arr.length;i++){ 
+            if(x!=0){  //sorting the numbers as per their value 
                 arr[i]=0;
                 x--;
             }
@@ -31,7 +31,7 @@ public class sortcolors {
             }
         }
     }
-    public static void main(String args[]){
+    public static void main(String args[]){ // main method to call the function
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int arr[]=new int[n];
@@ -39,7 +39,7 @@ public class sortcolors {
             arr[i]=sc.nextInt();
         }
         sort(arr);
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length;i++){ //printing the array in sorted order
             System.out.print(arr[i]+" ");
         }
     }
