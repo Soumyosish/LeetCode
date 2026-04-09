a04feec7a7a8820c8a89e5addb5631be50f4deec
SoumyosishPal <soumyosishpal.108@gmail.com>
2026-04-09 22:40:35 +0530

    new problem added

---
+++ Normal Problems/linear search/linear.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class linear{
+    public static int linearsearch(int arr[],int target){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target)   return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        System.out.println(linearsearch(arr,target));
+    }
+}
