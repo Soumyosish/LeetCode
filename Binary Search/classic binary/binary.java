f704236ac850f58fea8908caedddd0292baa3415
SoumyosishPal <soumyosishpal.108@gmail.com>
2026-04-12 03:06:32 +0530

    binary search problems started

---
+++ Binary Search/classic binary/binary.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class binary {
+    public static int search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target)   return mid;
+            else if(nums[mid]<target) start=mid+1;
+            else    end=mid-1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] nums=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+        }
+        int target=sc.nextInt();
+        System.out.println(search(nums, target));
+    }
+}
